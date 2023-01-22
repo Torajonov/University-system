0da46e6d7c95b10896b70f56802e82e02129753d
Javlonbek <85381615+Torajonov@users.noreply.github.com>
2023-01-22 16:14:58 +0500

    update

--- University/main/views.py
+++ University/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import (
+	ListView,
+	DetailView,)
+from .models import *
 # Create your views here.
+
+class HomePageView(ListView):
+	template_name = 'index.html'
